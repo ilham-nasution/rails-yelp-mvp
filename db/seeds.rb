e6f2b444ad02ff194b26609634e98b54351a4d6b
Ilham Rahman <ilhaamrahman@gmail.com>
2019-11-06 15:27:29 +0900

    Create a Yelp MVP app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+nums = (1..18).to_a
+
+puts "Creating review...."
+nums.each do |id|
+  Review.create!(
+    content: Faker::Restaurant.review,
+    rating: [1, 2, 3, 4, 5].sample,
+    restaurant_id: id
+  )
+end
+puts "....finished"
